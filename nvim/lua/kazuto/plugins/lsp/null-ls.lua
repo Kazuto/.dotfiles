19f7e206fea1a02fc640df74db170d2a14ec7a4a
Kazuto <mail@kazuto.de>
2023-06-22 09:09:54 +0200

    perf(nvim): use eslint_d and prettierd in null_ls

--- nvim/lua/kazuto/plugins/lsp/null-ls.lua
+++ nvim/lua/kazuto/plugins/lsp/null-ls.lua
@@ -15,23 +15,46 @@ local diagnostics = null_ls.builtins.diagnostics
 null_ls.setup({
 	debug = false,
 	sources = {
-		code_actions.eslint,
+		-- eslint_d
+		code_actions.eslint_d.with({
+			condition = function(utils)
+				return utils.root_has_file({ ".eslintrc.js" })
+			end,
+		}),
+		diagnostics.eslint_d.with({
+			condition = function(utils)
+				return utils.root_has_file({ ".eslintrc.js" })
+			end,
+		}),
+		formatting.eslint_d.with({
+			condition = function(utils)
+				return utils.root_has_file({ ".eslintrc.js" })
+			end,
+		}),
+
 		code_actions.shellcheck,
 
 		diagnostics.actionlint,
 		diagnostics.codespell,
 		diagnostics.dotenv_linter,
 		diagnostics.editorconfig_checker,
-		diagnostics.eslint,
-		diagnostics.phpstan,
+		-- diagnostics.luacheck.with({
+		-- 	extra_args = { "--config", vim.fn.stdpath("config") .. "/.luacheckrc" },
+		-- }),
+
+		-- diagnostics.phpstan,
 		diagnostics.shellcheck,
+		diagnostics.trail_space.with({
+			disabled_filetypes = { "NvimTree" },
+		}),
 
 		formatting.beautysh,
-		formatting.fixjson,
-		formatting.eslint,
+		formatting.jq,
 		formatting.lua_format,
 		formatting.pint,
-		formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
+		formatting.prettierd.with({
+			extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
+		}),
 		formatting.stylua,
 	},
 })
