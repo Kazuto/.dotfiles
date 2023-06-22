e140f5077dbf27bc5404b4a1487aab204774fbeb
Kazuto <mail@kazuto.de>
2023-06-22 09:10:30 +0200

    fix(nvim): add file and buf ignore to indent_blankline

--- nvim/lua/kazuto/plugins/indent-blankline.lua
+++ nvim/lua/kazuto/plugins/indent-blankline.lua
@@ -4,6 +4,19 @@ if not status then
 end
 
 indent_blankline.setup({
+	filetype_exclude = {
+		'help',
+		'terminal',
+		'dashboard',
+		'lspinfo',
+		'TelescopePrompt',
+		'TelescopeResults',
+	},
+	buftype_exclude = {
+		'terminal',
+		'NvimTree',
+	},
 	char = "┊",
 	show_trailing_blankline_indent = false,
+	show_first_indent_level = true,
 })
